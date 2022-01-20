edcbfcb31b40b16e489f1a34cf28923cce632a60
evaldez <edgarvaldez99@gmail.com>
2022-01-20 17:41:34 -0300

    fix: orden_carga_seeds

--- app/database/populate/orden_carga_seeds.py
+++ app/database/populate/orden_carga_seeds.py
@@ -4,20 +4,23 @@ from typing import Optional
 
 from sqlalchemy.orm import Session  # type: ignore
 
-from app.repositories import (
-    get_camion_list_by_gestor_cuenta_id,
-    get_flete_list_by_gestor_carga_id,
-)
+from app.repositories import get_flete_list_by_gestor_carga_id
 from app.schemas import OrdenCargaForm
-from app.services import create_orden_carga, get_semi_list_by_camion_id_and_producto_id
+from app.services import (
+    create_orden_carga,
+    get_camion_list_by_producto_id,
+    get_semi_list_by_camion_id_and_producto_id,
+)
 
 
 def orden_carga_seeds(db: Session, gestor_carga_id: Optional[int]):
     modified_by = "system"
     if gestor_carga_id:
         fletes = get_flete_list_by_gestor_carga_id(db, gestor_carga_id)
-        camiones = get_camion_list_by_gestor_cuenta_id(db, gestor_carga_id)
         for flete in fletes:
+            camiones = get_camion_list_by_producto_id(
+                db, flete.producto_id, gestor_carga_id
+            )
             for camion in camiones:
                 semis = get_semi_list_by_camion_id_and_producto_id(
                     db, camion.id, flete.producto_id, gestor_carga_id
