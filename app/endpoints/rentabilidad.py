160d84205ed03098f592c64038121f095cfa6cf3
evaldez <edgarvaldez99@gmail.com>
2022-04-23 23:53:17 -0400

    feat: added rentabilidad report

--- app/endpoints/rentabilidad.py
+++ app/endpoints/rentabilidad.py
@@ -18,3 +18,12 @@ async def read_rentabilidad_list(
     _: bool = Depends(Permiso(a.LISTAR, m.RENTABILIDAD)),  # noqa: B008
 ):
     return services.get_rentabilidad_list(db, current_user.gestor_carga_id)
+
+
+@api.get("/reports")
+async def rentabilidad_reports(
+    db: Session = Depends(get_db_session),  # noqa: B008
+    current_user: models.User = Depends(get_current_user),  # noqa: B008
+    _: bool = Depends(Permiso(a.REPORTE, m.RENTABILIDAD)),  # noqa: B008
+):
+    return services.get_rentabilidad_reports(db, current_user.gestor_carga_id)
