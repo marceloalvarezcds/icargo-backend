844446d94f4d49e82ca623ad900418aec013192f
evaldez <edgarvaldez99@gmail.com>
2022-04-26 13:15:12 -0400

    refactor: composicion juridica seeds

--- app/database/seeds/composicion_juridica_seeds.py
+++ app/database/seeds/composicion_juridica_seeds.py
@@ -1,19 +1,24 @@
-from sqlalchemy.exc import IntegrityError  # type: ignore
 from sqlalchemy.orm import Session  # type: ignore
 
 from app.models import ComposicionJuridica
+from app.repositories import get_composicion_juridica_by_nombre
+
+
+def create_composicion_juridica(db: Session, nombre: str, nombre_corto: str):
+    composicion_juridica = get_composicion_juridica_by_nombre(db, nombre)
+    if composicion_juridica is None:
+        composicion_juridica = ComposicionJuridica(
+            nombre=nombre, nombre_corto=nombre_corto
+        )
+        db.add(composicion_juridica)
+        db.commit()
 
 
 def composicion_juridica_seeds(db: Session):
-    try:
-        db.add(ComposicionJuridica(nombre="Uni-personal", nombre_corto="U.P."))
-        db.add(ComposicionJuridica(nombre="Sociedad Anónima", nombre_corto="S.A."))
-        db.add(
-            ComposicionJuridica(
-                nombre="Sociedad de Responsabilidad Limitada", nombre_corto="S.R.L."
-            )
-        )
-        db.add(ComposicionJuridica(nombre="Sociedad Cooperativa", nombre_corto="S.C."))
-        db.commit()
-    except IntegrityError:
-        db.rollback()
+    create_composicion_juridica(db, nombre="Sociedad Anónima", nombre_corto="S.A.")
+    create_composicion_juridica(
+        db, nombre="Sociedad de Responsabilidad Limitada", nombre_corto="S.R.L."
+    )
+    create_composicion_juridica(
+        db, nombre="Sociedad Anónima Comercial Industrial.", nombre_corto="S.A.C.I."
+    )
