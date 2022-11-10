2f93fe717440410b11c53a1afbf3bf3365ce5e96
Edgar Valdez <edgarvaldez99@gmail.com>
2022-11-10 13:46:29 -0300

    refactor: added audit colums in OC tables

--- app/schemas/orden_carga_descuento.py
+++ app/schemas/orden_carga_descuento.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from pydantic import BaseModel
 
+from .date_model import Date
 from .moneda import Moneda
 from .proveedor import Proveedor
 from .rounded_decimal_model import RoundedDecimal
@@ -25,6 +26,11 @@ class OrdenCargaDescuentoForm(BaseModel):
     # FIN Monto a pagar al Proveedor
     orden_carga_id: int
     flete_id: Optional[int] = None
+    # Auditoría
+    created_by: str
+    created_at: Date
+    modified_by: str
+    modified_at: Date
 
 
 class OrdenCargaDescuento(OrdenCargaDescuentoForm):
