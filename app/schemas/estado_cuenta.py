01f0c7b0ab3225605097a54bacb57cfd2960e0ba
bruno_brizuela <bruno_brizuela@hacienda.gov.py>
2024-10-09 14:53:21 -0300

    fix estado cc y add nuevo servicio get saldo cc contraparte

--- app/schemas/estado_cuenta.py
+++ app/schemas/estado_cuenta.py
@@ -64,3 +64,17 @@ class EstadoCuenta(BaseModel):
         for row in results:
             list_to_return.append(cls.from_orm_row(row))
         return list_to_return
+
+
+class ContraparteEstadoCuenta(BaseModel):
+    confirmado: RoundedDecimal
+    finalizado: RoundedDecimal
+
+    @classmethod
+    def from_dict(cls, dic: Dict) -> "ContraparteEstadoCuenta":
+        return ContraparteEstadoCuenta(**dic)
+
+    @classmethod
+    def from_orm_row(cls, row: Row) -> "ContraparteEstadoCuenta":
+        dic = row._asdict()
+        return cls.from_dict(dic)
--- app/services/__init__.py
+++ app/services/__init__.py
@@ -63,7 +63,8 @@ from .estado_cuenta import (  # noqa
     get_estado_cuenta_by_contraparte,
     get_estado_cuenta_list,
     get_estado_cuenta_reports,
-    get_nuevo_servicio
+    get_nuevo_servicio,
+    get_saldo_cuenta_contraparte,
 )
 from .factura import (  # noqa
     create_factura,
