3f3b26eb30e9d1447c072acd1d7bf38678b91fa4
bruno_brizuela <bruno_brizuela@hacienda.gov.py>
2024-10-31 13:51:11 -0300

    add registro tipo mov fiscal para movimientos automaticos de la factura

---
+++ alembic/versions/58e82ce16ce9_insert_tipo_mov_fiscal.py
@@ -0,0 +1,26 @@
+"""insert_tipo_mov_fiscal
+
+Revision ID: 58e82ce16ce9
+Revises: 23212a8f4111
+Create Date: 2024-10-31 13:27:32.496319
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '58e82ce16ce9'
+down_revision = '23212a8f4111'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute('insert into tipo_movimiento '\
+        ' (created_by, created_at, modified_by, modified_at, id, descripcion, estado, cuenta_id, codigo ) '\
+        ' values (\'system\', now(), \'system\', now(), 7, \'Fiscal\', \'Activo\', 1, \'07\' )' )
+
+
+def downgrade():
+    op.execute('delete from tipo_movimiento where descripcion = \'Fiscal\' ')
