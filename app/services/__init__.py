346d08c19a3d9c26dbeae3aa01dfeb1cdcdf9cd4
bruno_brizuela <bruno_brizuela@hacienda.gov.py>
2024-12-20 17:30:31 -0300

    fix datos pendientes remision oc documento

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -229,8 +229,7 @@ from .orden_carga import (  # noqa
     get_ordenes_carga_by_combinacion_id_and_aceptado,
     get_ordenes_carga_by_combinacion_id_and_nuevo,
     update_comentarios,
-    create_orden_carga_comentarios_historial,
-    edit_documento_fisico_orden_carga,
+    create_orden_carga_comentarios_historial
 )
 from .orden_carga_anticipo_retirado import (  # noqa
     create_orden_carga_anticipo_retirado,
