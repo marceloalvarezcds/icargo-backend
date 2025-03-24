7bf12c416be45796ddd0a770482723e97768b0b4
Dario <ortiz.dario94@gmail.com>
2025-03-24 11:31:11 -0400

    Fix internal server error oc

--- app/services/orden_carga_remision_resultado.py
+++ app/services/orden_carga_remision_resultado.py
@@ -87,7 +87,7 @@ def get_orden_carga_remision_resultado_list_by_flete(
                     merma_valor_total_moneda_local=(
                         orden_carga.resultado_flete_gestor_carga_merma_valor_total_moneda_local
                     ),
-                    saldo=orden_carga.resultado_flete_gestor_carga_total_flete,
+                    saldo=orden_carga.resultado_flete_gestor_carga_saldo,
                     complemento_descuento = orden_carga.resultado_gestor_carga_complemento_descuento,
 
                     # resultado_flete_gestor_carga_saldo (calculo anterior),
