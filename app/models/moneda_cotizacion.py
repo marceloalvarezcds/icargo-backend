60520adc1103c195fcd50c9c548a116a50016eca
Dario <ortiz.dario94@gmail.com>
2025-03-31 15:51:36 -0300

    Guardar cambios locales en moneda_cotizacion.py

--- app/models/moneda_cotizacion.py
+++ app/models/moneda_cotizacion.py
@@ -15,7 +15,6 @@ from app.audits.audit_mixin import AuditMixin
 from app.database.base import Base
 from app.enums.estado import EstadoEnum
 from app.models.gestor_carga import GestorCarga
-from app.models.moneda import Moneda
 
 
 class MonedaCotizacion(AuditMixin, Base):
@@ -27,9 +26,14 @@ class MonedaCotizacion(AuditMixin, Base):
     id = Column(Integer, primary_key=True)
     gestor_carga_id = Column(Integer, ForeignKey("gestor_carga.id"))
     gestor_carga = relationship(GestorCarga, uselist=False)
+
     moneda_origen_id = Column(Integer, ForeignKey("moneda.id"))
     moneda_destino_id = Column(Integer, ForeignKey("moneda.id"))
-    moneda = relationship(Moneda, uselist=False)
+
+    # 🔹 Relación correcta, separando origen y destino
+    moneda_origen = relationship("Moneda", foreign_keys=[moneda_origen_id])
+    moneda_destino = relationship("Moneda", foreign_keys=[moneda_destino_id])
+
     fecha = Column(DateTime)
     estado = Column(String(255), server_default=EstadoEnum.ACTIVO.value)
     cotizacion_moneda = Column(Numeric(38, 10))
