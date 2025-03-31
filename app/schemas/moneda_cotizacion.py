b7846d0cbe12d04c21db19a203fe6a333f49a702
Dario <ortiz.dario94@gmail.com>
2025-03-31 16:05:45 -0300

    Fix conflicto schema moneda_cotizacion

--- app/schemas/moneda_cotizacion.py
+++ app/schemas/moneda_cotizacion.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-from datetime import datetime
-from typing import Optional
-
-from app.schemas.rounded_decimal_model import RoundedDecimal
-from .estado_base_model import EstadoBaseModel
-
-
-class MonedaCotizacion(EstadoBaseModel):
-=======
 from typing import Optional
 from pydantic import BaseModel
 from .rounded_decimal_model import RoundedDecimal
@@ -15,18 +5,12 @@ from .moneda import Moneda
 
 
 class MonedaCotizacion(BaseModel):
->>>>>>> 3a4c593dbb1a6973b45b3727d4cd7fbb0f3981c8
     id: int
     gestor_carga_id: int
     moneda_origen_id: int
     moneda_destino_id: int
-<<<<<<< HEAD
-    fecha: Optional[datetime] = None
-=======
-    #moneda: Moneda
     fecha: str
     estado: str
->>>>>>> 3a4c593dbb1a6973b45b3727d4cd7fbb0f3981c8
     cotizacion_moneda: RoundedDecimal
 
     class Config:
