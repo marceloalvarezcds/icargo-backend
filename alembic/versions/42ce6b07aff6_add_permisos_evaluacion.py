bf6bbf80733e3cd87f44b7041675ab7cd514c56b
bruno_brizuela <bruno_brizuela@hacienda.gov.py>
2025-05-22 10:36:34 -0300

    fix permisos evaluacion 2

--- alembic/versions/42ce6b07aff6_add_permisos_evaluacion.py
+++ alembic/versions/42ce6b07aff6_add_permisos_evaluacion.py
@@ -27,11 +27,11 @@ def upgrade():
     ##    "(modified_by, modified_at, accion, modelo, descripcion, created_by, created_at, modulo, modelo_titulo, is_for_superuser) VALUES "
     ##    "('system', now(), 'crear', 'orden_carga_evaluacion', 'Crear 10 - evaluacion', 'system', now(), '5 - Orden de Carga', 'Evaluacion', false)"
     ##)
-    op.execute(
-        "INSERT INTO permiso "
-        "(modified_by, modified_at, accion, modelo, descripcion, created_by, created_at, modulo, modelo_titulo, is_for_superuser) VALUES "
-        "('system', now(), 'ver', 'orden_carga_evaluacion', 'Ver 10 - evaluacion', 'system', now(), '5 - Orden de Carga', 'Evaluacion', false)"
-    )
+    ##op.execute(
+    ##    "INSERT INTO permiso "
+    ##    "(modified_by, modified_at, accion, modelo, descripcion, created_by, created_at, modulo, modelo_titulo, is_for_superuser) VALUES "
+    ##    "('system', now(), 'ver', 'orden_carga_evaluacion', 'Ver 10 - evaluacion', 'system', now(), '5 - Orden de Carga', 'Evaluacion', false)"
+    ##)
 
 def downgrade():
     op.execute("delete from permiso where modelo = 'orden_carga_evaluacion' and accion = 'listar'")
